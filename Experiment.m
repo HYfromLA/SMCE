af737fbe4b205e17b39b21ddb9a69b3d58eb90b8
HYfromLA <106124968+HYfromLA@users.noreply.github.com>
2024-04-29 20:07:30 -0600

    Add files via upload

--- Experiment.m
+++ Experiment.m
@@ -1,11 +1,13 @@
 DATAopts.Shape = 'Dollar Sign';         DATAopts.Number = [7000, 3000];         % 4
 DATAopts.AmbDim = 10;                    DATAopts.NoiseSigma = 0.030;
 
-[X, LabelsGT] = SimData(DATAopts, 1);
+[X, LabelsGT] = simdata(DATAopts, 1);
 lambdas = [1 5 10 20 50]; KMaxs = [5 10 20 30 40 50 80 100];
 smce_tune = []; 
 for i=1:length(lambdas)
+    i
     for j=1:length(KMaxs)
+        j
         [Yc,Yj,clusters,missrate] = smce(X',lambdas(i),KMaxs(j),2,max(LabelsGT),LabelsGT,false);
         smce_tune=[smce_tune; [lambdas(i) KMaxs(j) 1-missrate]]; 
     end
